0437582ace82ec7e42b12aeb6bdc292232db6f7b
John Macharia <machariajohn23@gmail.com>
2022-06-02 10:49:27 -0700

    Implement a method in a class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    }
+  }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
