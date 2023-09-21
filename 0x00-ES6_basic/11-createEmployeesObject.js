d9b7977c56d86b361149a7b602f6d584ffdbdedd
John Macharia <machariajohn23@gmail.com>
2023-09-22 00:24:49 +0300

    Function that returns an object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,8 @@
 export default function createEmployeesObject(departmentName, employees) {
+  const newObj = {};
+  newObj.departmentName = employees;
+  return newObj;
+  /* OR create the new obj as shown below
   return { [departmentName]: [...employees] };
+  */
 }
