1175f41694203ca0668395f05743b72a7f0d3f93
John Macharia <machariajohn23@gmail.com>
2023-09-30 01:04:59 +0300

    Return a promise from a function, depending on the value passed in as the function argument

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-export default function getFullResponseFromAPI (success) {
+export default function getFullResponseFromAPI(success) {
   if (success === true) {
     return Promise.resolve({ status: 200, body: 'Success' });
   }
