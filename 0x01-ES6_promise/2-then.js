dd8afa3c83c8b4531d00b3a78c4f0d1310a015c0
John Macharia <machariajohn23@gmail.com>
2023-09-30 01:58:59 +0300

    Append then, catch and finally handlers to the promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,6 +2,6 @@ export default function handleResponseFromAPI(promise) {
   const obj = { status: 200, body: 'success' };
   return promise
     .then(() => obj)
-    .catch(() => console.error())
+    .catch((error) => error)
     .finally(() => console.log('Got a response from the API'));
 }
