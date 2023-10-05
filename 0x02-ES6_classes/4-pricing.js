a769ed8bec23c792a61d2ef4e4cc96af2e52194e
John Macharia <machariajohn23@gmail.com>
2023-10-05 10:15:52 +0300

    Inherit from a parent class and use an instance of the parent class as an attribute in the constructor of the child class

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,29 +1,36 @@
 import Currency from './3-currency';
 
-export default class Pricing {
+export default class Pricing extends Currency {
   constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+    super();
+    delete this._code;
+    delete this._name;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(newAmount) {
-    this._amount = newAmount;
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    }
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(newCurrency) {
-    this._currency = newCurrency;
+  set currency(value) {
+    if (value instanceof 'Currency') {
+      this._currency = value;
+    }
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
