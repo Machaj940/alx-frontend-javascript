076ab161b736f2d70398b59773c09fdfe26af87e
John Macharia <machariajohn23@gmail.com>
2023-10-05 11:30:07 +0300

    Make sure a class that extends from an abstract class implements the method in the abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,8 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
@@ -7,11 +10,7 @@ export default class Building {
     return this._sqft;
   }
 
-  set sqft(value) {
-    this._sqft = value;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage')
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 }
